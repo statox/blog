c0269ec76be37943cc28e0ec002ef8c77b06d04c
Adrien Fabre <adrien.fabre@dashlane.com>
2021-10-31 21:58:03 +0100

    pass parameters to github action

--- .github/actions/add-song-chords-action/index.js
+++ .github/actions/add-song-chords-action/index.js
@@ -3,14 +3,17 @@ const github = require('@actions/github');
 
 try {
     console.log('Running add-song-chords-action index.js');
-    console.log(core.getInput('issueId'));
-    console.log(core.getInput('login'));
-    console.log(core.getInput('commenterId'));
-    console.log(core.getInput('comment'));
+    console.log({issueId: core.getInput('issueId')});
+    console.log({login: core.getInput('login')});
+    console.log({commenterId: core.getInput('commenterId')});
+    console.log({comment: core.getInput('comment')});
+
+    const event = JSON.stringify(github.event, undefined, 2);
+    console.log(`The event event: ${event}`);
 
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
+    //const payload = JSON.stringify(github.context.payload, undefined, 2);
+    //console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
