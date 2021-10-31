10f7772058cd8f8a772e3af04ad2ae8c41444efe
Adrien Fabre <adrien.fabre@dashlane.com>
2021-10-31 22:12:14 +0100

    Make the comment action work

--- .github/actions/add-song-chords-action/index.js
+++ .github/actions/add-song-chords-action/index.js
@@ -1,19 +1,38 @@
+const {exec} = require('child_process');
+const fs = require('fs');
 const core = require('@actions/core');
 const github = require('@actions/github');
+const FILE_URL = 'src/_data/chords.json';
 
-try {
-    console.log('Running add-song-chords-action index.js');
-    console.log({issueId: core.getInput('issueId')});
-    console.log({login: core.getInput('login')});
-    console.log({commenterId: core.getInput('commenterId')});
-    console.log({comment: core.getInput('comment')});
+// const artist = 'Dropkick Murphys';
+// const title = 'Rose tattoo';
+// const url = 'https://www.tabs4acoustic.com/en/guitar-tabs/dropkick-murphys-tabs/rose-tattoo-acoustic-tab-389.html';
 
-    const event = JSON.stringify(github.event, undefined, 2);
-    console.log(`The event event: ${event}`);
+const body = core.getInput('comment');
+const [artist, title, url] = body.split('\r\n');
 
-    // Get the JSON webhook payload for the event that triggered the workflow
-    //const payload = JSON.stringify(github.context.payload, undefined, 2);
-    //console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
-}
+console.log({artist, title, url});
+
+exec(`tail -n +2 ${FILE_URL}`, (error, filecontent, stderr) => {
+    if (error) {
+        core.setFailed(error.message);
+        exit(1);
+    }
+
+    const item = JSON.stringify({artist, title, url}, {space: 4}, 4).replace(/^/g, '[I]');
+    const newContent =
+        '[\n' +
+        '    {\n' +
+        `        "artist": "${artist}",\n` +
+        `        "title": "${title}",\n` +
+        `        "url": "${url}"\n` +
+        '    },\n' +
+        filecontent;
+
+    fs.writeFileSync(FILE_URL, newContent);
+
+    exec(`git diff ${FILE_URL}`, (error, out) => {
+        console.log('output of git diff');
+        console.log(out);
+    });
+});
