d2e339e797dad28dc42a86865069ad9eab21fc77
statox <me@statox.fr>
2022-07-15 12:47:25 +0200

    Add ability to get back previous random song in chords page

--- src/_includes/scripts/randomChordsButton.js
+++ src/_includes/scripts/randomChordsButton.js
@@ -6,7 +6,12 @@
 const CHORDS_URL = 'https://www.statox.fr/chords.json';
 // const CHORDS_URL = 'http://localhost:8080/chords.json';
 let chords_data;
+
+// Initially pastSelections didn't exist and we only used the set to avoid suggesting the same song twice
+// pastSelections was added to make the getPreviousRandomSong button work but I'm too lazy to remove alreadySeen properly
+// TODO when I feel like doing it
 const alreadySeen = new Set();
+const pastSelections = [];
 
 fetch(CHORDS_URL)
     .then(response => response.json())
@@ -32,9 +37,28 @@ function getRandomSong() {
         if (!alreadySeen.has(song.url)) {
             randomSong = song;
             alreadySeen.add(song.url);
+            pastSelections.push(song);
         }
     }
 
+    updateSelection();
+}
+
+function getPreviousRandomSong() {
+    if (pastSelections.length <= 1) {
+        return;
+    }
+    const deleted = pastSelections.pop();
+    alreadySeen.delete(deleted.url);
+    updateSelection();
+}
+
+function updateSelection() {
+    if (pastSelections.length === 0) {
+        return;
+    }
+
+    const randomSong = pastSelections[pastSelections.length - 1];
     const title = `${randomSong.artist} - ${randomSong.title} `;
     const a = document.createElement('a');
     const linkText = document.createTextNode(title);
